6a1e06d1d2d92c7ba63143b1115998bd54cc180b
Gabi Muñoz <uc10gabriela052@gmail.com>
2025-05-09 00:27:26 -0300

    etapa 3 concluida

--- Etapa 3/Etapa3.java
+++ Etapa 3/Etapa3.java
@@ -1,7 +1,22 @@
 import java.util.Stack;
 
-public class Etapa3 {
+public class Etapa3{
+
     public boolean checkBrackets(Stack<Character> s1){
-        
+        Stack<Character> pilhaParenteses = new Stack<>();
+
+        while(!s1.isEmpty()){
+            char caracter = s1.pop();
+            if(caracter == '('){
+                pilhaParenteses.push(caracter);
+            }
+            else if(caracter == ')'){
+                if(pilhaParenteses.isEmpty()){
+                    return false;
+                }
+                pilhaParenteses.pop();
+            }
+        }
+        return pilhaParenteses.isEmpty();
     }
 }
